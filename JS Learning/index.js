979bb4557667ddb62e22ad76c0c4a5a06ee4708e
Felipe <fcastro96@hotmail.com>
2024-01-12 14:52:45 -0300

    first commit

---
+++ JS Learning/index.js
@@ -0,0 +1,6 @@
+console.log("Les go 'yay'"); // string
+console.log('Les go');
+console.log(`Les go`);
+//uso de '' "" e `` para strings (textos)
+
+console.log(123456, 15.85); // number
---
+++ JS Learning/p1/app.js
@@ -0,0 +1,1 @@
+console.log("Oh shit, here we go again");
