c3d3deeb3a10bbb5e91c93d3d647ec859df2831f
Felipe <fcastro96@hotmail.com>
2024-01-15 15:33:27 -0300

    dados primitivos

---
+++ JS Learning/p9DadosPrimitivos/index.js
@@ -0,0 +1,8 @@
+const nome = "felipe"; //string
+const nome2 = "felipe2"; //string
+const nome3 = `felipe3`; //string
+const num1 = 10; //number
+const num2 = 10.55; //number
+let nomeEstudante; //undefined
+const sobrenomeEstudante = null; // Nulo
+const aprovado = true // boolean
