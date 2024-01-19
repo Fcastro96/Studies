36be21378846caaf7bfd252f006eb819f5579e10
Felipe <fcastro96@hotmail.com>
2024-01-19 18:26:20 -0300

    feat: operadores aritmeticos

---
+++ JS Learning/p10opAritmeticos/index.js
@@ -0,0 +1,49 @@
+const num1 = 5;
+const num2 = 10;
+const num3 = 2;
+const num4 = 3;
+//console.log(num1 + num2);
+//console.log(num3 ** num2);
+
+// dois asteriscos (**) = potenciação
+// (%) = resto da divisão entre dois numeros
+
+//console.log(num2 % num4);
+
+//operador de incremento ++
+// let contador = 1;
+// contador++; //2
+// ++contador; //3
+// contador++; //4
+// ++contador; //5
+// console.log(contador++);
+// console.log(contador);
+// console.log(++contador);
+// console.log(--contador);
+
+/*
+Incremento ++
+Decremento --
+*/
+
+//Operadores de atribuição
+const passo = 2;
+let contador2 = 0;
+// contador2 = contador2 + passo;
+// console.log(contador2);
+// contador2 = contador2 + passo;
+// console.log(contador2);
+// contador2 = contador2 + passo;
+// console.log(contador2);
+const num5 = parseInt("5");
+const num6 = parseFloat("5.2");
+contador2 += 2;
+contador2 += 2;
+contador2 += 2;
+console.log(contador2);
+contador2 *= 2;
+console.log(contador2);
+contador2 **= 2;
+console.log(contador2);
+console.log(typeof num5);
+console.log(typeof num6);
