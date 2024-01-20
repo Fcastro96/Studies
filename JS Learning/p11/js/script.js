ffc3b464c910c5ac48ea7f9f1694e2e1ac9c5e6e
Felipe <fcastro96@hotmail.com>
2024-01-20 17:14:18 -0300

    feat:Comandos no navegador

---
+++ JS Learning/p11/js/script.js
@@ -0,0 +1,8 @@
+// alert("New message");
+// confirm("Seu nome é rodolfo?");
+// prompt("Digite seu nome");
+// const confirmar = confirm("Você deseja deletar?");
+const num1 = prompt("Digite um numero");
+const num2 = prompt("Digite um numero");
+const resultado = parseFloat(num1) + parseFloat(num2);
+alert(`A soma dos dois valores informados é ${resultado}`);
