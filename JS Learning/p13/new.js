17bbbda7dce6af04e60dc91954b9a67918cfb363
Felipe <fcastro96@hotmail.com>
2024-01-21 19:41:51 -0300

    feat:mais sobre strings

---
+++ JS Learning/p13/new.js
@@ -0,0 +1,13 @@
+let umaString = 'Um "texto" imaculado na babilonia';
+console.log(umaString);
+console.log(umaString[6]);
+console.log(umaString.charAt(6));
+console.log(umaString.concat(" ", "Feliz"));
+console.log(umaString.lastIndexOf("t"));
+console.log(umaString.replace("Um", "Pato"));
+console.log(umaString.length);
+console.log(umaString.slice(7, 23));
+console.log(umaString.slice(-5));
+console.log(umaString.split("a"));
+console.log(umaString.toUpperCase(""));
+console.log(umaString.toLowerCase(""));
