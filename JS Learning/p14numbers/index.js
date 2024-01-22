06a434bf1ff34b802b7190241aa644289f256b90
Felipe <fcastro96@hotmail.com>
2024-01-22 17:43:14 -0300

    feat:numbers

---
+++ JS Learning/p14numbers/index.js
@@ -0,0 +1,18 @@
+let num1 = 10; // Number
+let num2 = 2.5; // Number
+let num3 = 1500;
+let num4 = 10.684654161;
+let num5 = 0.7;
+let num6 = 0.1;
+
+num5 += num6; // num5 = num5 + num6;
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1 + num2);
+// console.log(num3.toString(2));
+// console.log(num4.toFixed(5));
+// console.log(Number.isInteger(num4));
+// console.log(Number.isNaN(temp));
+// let temp = num1 * "5";
+console.log(num5 + num6);
