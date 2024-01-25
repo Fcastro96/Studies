34f6c9bc0c2621bd00bb3dfac3053c5692d663af
Felipe <fcastro96@hotmail.com>
2024-01-25 18:41:45 -0300

    feat:exNumbers

--- JS Learning/p15/index.js
+++ JS Learning/p15/index.js
@@ -1,16 +1,16 @@
-// let num1 = 9.39646;
-// let num2 = Math.floor(num1);
-// let num3 = Math.ceil(num1);
-// let num4 = Math.round(num1);
-// console.log(num2);
-// console.log(num3);
-// console.log(num4);
-// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500));
-// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500));
-// console.log(Math.random());
-// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
-// console.log(aleatorio);
-// console.log(Math.pow(2, 10)); //potenciação
+ let num1 = 9.39646;
+ let num2 = Math.floor(num1);
+ let num3 = Math.ceil(num1);
+ let num4 = Math.round(num1);
+ console.log(num2);
+ console.log(num3);
+ console.log(num4);
+ console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500));
+ console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500));
+ console.log(Math.random());
+ const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+ console.log(aleatorio);
+ console.log(Math.pow(2, 10)); //potenciação
 
 let num5 = 25;
 console.log(num5 ** 0.5);
