8b69e864f1afc8a7a543ccd67e9cbbc39f41b9b3
Felipe <fcastro96@hotmail.com>
2024-01-26 18:29:03 -0300

    Retirada

--- JS Learning/tests.js
+++
@@ -1,4 +0,0 @@
-console.log("Felipe está se esforçando");
-console.log("Felipe está se esforçando de verdade");
-console.log("Felipe está se esforçando em 2024");
-console.log("Oh shit, here we go again");
