8ca4a456f04c84e9e5c06170b6e9042b1c002972
Felipe <fcastro96@hotmail.com>
2024-02-08 16:07:17 -0300

    feat: ex com for

--- JS Learning/1 Testando Conhecimentos/1for.js
+++ JS Learning/1 Testando Conhecimentos/1for.js
@@ -2,6 +2,8 @@
 //   console.log(index);
 // }
 
+//--------------------------------------------------------------
+
 // const n = 10;
 // const i = 1;
 // let soma = 0;
@@ -11,10 +13,29 @@
 //   console.log(`${soma} + ${index} = ${(soma += index)}`);
 // }
 
-const n = 20;
-const i = 1;
-let subtrair = 20;
+//--------------------------------------------------------------
 
-for (let index = i; index <= n; index++) {
-  console.log(`${subtrair} - ${index} = ${subtrair -= index}`);
+// const n = 20;
+// const i = 1;
+// let subtrair = 20;
+
+// for (let index = i; index <= n; index++) {
+//   console.log(`${subtrair} - ${index} = ${subtrair -= index}`);
+// }
+
+//--------------------------------------------------------------
+
+// const n = 10;
+// const i = 1;
+// let multi = 7;
+
+// for (let index = i; index <= 10; index++) {
+//   console.log(`${multi} * ${index} = ${multi * index}`);
+// }
+
+//--------------------------------------------------------------
+const n = 10;
+
+for (let i = 1; i <= n; i++) {
+  console.log(`O quadrado de ${i} é ${i ** 2} e o cubo de ${i} é ${i ** 3}`);
 }
--- JS Learning/p14numbers/index.js
+++ JS Learning/p14numbers/index.js
@@ -7,12 +7,12 @@ let num6 = 0.1;
 
 num5 += num6; // num5 = num5 + num6;
 
-// console.log(num1.toString() + num2);
+//console.log(num1.toString() + num2);
 // num1 = num1.toString();
-// console.log(num1 + num2);
-// console.log(num3.toString(2));
-// console.log(num4.toFixed(5));
-// console.log(Number.isInteger(num4));
-// console.log(Number.isNaN(temp));
-// let temp = num1 * "5";
-console.log(num5 + num6);
+ console.log(num1 + num2);
+ console.log(num3.toString(2));
+ console.log(num4.toFixed(5));
+ console.log(Number.isInteger(num4));
+ console.log(Number.isNaN(temp));
+//let temp = num1 * "5";
+//console.log(num5 + num6);
