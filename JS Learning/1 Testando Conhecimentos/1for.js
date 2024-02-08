074955a533c560383370267561d7ee8f43dd510e
Felipe <fcastro96@hotmail.com>
2024-02-08 00:33:07 -0300

    feat: ex for

---
+++ JS Learning/1 Testando Conhecimentos/1for.js
@@ -0,0 +1,20 @@
+// for (let index = 10; index >= 1; index--) {
+//   console.log(index);
+// }
+
+// const n = 10;
+// const i = 1;
+// let soma = 0;
+
+// for (let index = 1; index <= n; index++) {
+//   soma += index;
+//   console.log(`${soma} + ${index} = ${(soma += index)}`);
+// }
+
+const n = 20;
+const i = 1;
+let subtrair = 20;
+
+for (let index = i; index <= n; index++) {
+  console.log(`${subtrair} - ${index} = ${subtrair -= index}`);
+}
