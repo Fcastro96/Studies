b5a4449cf9e06e896e76190b6089228b12382923
Felipe <fcastro96@hotmail.com>
2024-04-22 17:31:02 -0300

    extrutura de repetição for

---
+++ JS Learning/p34/index.js
@@ -0,0 +1,17 @@
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+// }
+
+// i = index
+// Primeira estrutura de repetição
+// for (let i = 0; i <= 10; i++) {
+//   const par = i % 2 === 0 ? "par" : "impar";
+//   console.log(i, par);
+// }
+
+// Segunda estrutura de repetição
+const frutas = ["Maçã", "Pêra", "Uva"];
+
+for (let i = 0; i < frutas.length; i++) {
+console.log(`Índice ${i}`, frutas[i]);
+}
